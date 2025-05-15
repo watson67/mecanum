715ebc4b6e4d463a13d2833613c53d7ad025f364
GusBon <augustin.bonnel@insa-strasbourg.fr>
2025-05-15 10:56:18 +0200

    Nettoyage

--- +bus_conv_fcns/+ros2/+msgToBus/+geometry_msgs/Vector3.m
+++
@@ -1,7 +0,0 @@
-function slBusOut = Vector3(msgIn, slBusOut, varargin)
-%#codegen
-%   Copyright 2021-2022 The MathWorks, Inc.
-    slBusOut.x = double(msgIn.x);
-    slBusOut.y = double(msgIn.y);
-    slBusOut.z = double(msgIn.z);
-end
--- src/build/teleop_mecanum/build/lib/teleop_mecanum/__init__.py
+++
@@ -1,1 +0,0 @@
-# Ce fichier est nécessaire pour que le répertoire soit reconnu comme un package Python
