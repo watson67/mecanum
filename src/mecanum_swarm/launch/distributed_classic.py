d8d4cd1eccd88eca30c072d856690c0e58dfff46
GusBon <augustin.bonnel@insa-strasbourg.fr>
2025-05-27 16:22:17 +0200

    update launch

--- src/mecanum_swarm/launch/distributed_classic.py
+++ src/mecanum_swarm/launch/distributed_classic.py
@@ -1,14 +1,21 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import socket
 
 #Ce launch file est à lancer sur chaque robot du swarm.
 def generate_launch_description():
+    hostname = socket.gethostname().lower()
+    # Optionnel : nettoyer le hostname si besoin (ex: enlever '-desktop')
+    if hostname.endswith('-desktop'):
+        hostname = hostname[:-8]
+    robot_name = hostname.capitalize()
+
     return LaunchDescription([
         # Publie les repères de transformation TF2 pour chaque robot
         Node(
             package='mecanum_swarm',
             executable='distributed_tf2_manager',
-            name='distributed_tf2_manager',
+            name=f'distributed_tf2_manager_{robot_name.lower()}',
             output='screen'
         ),
         
@@ -16,7 +23,7 @@ def generate_launch_description():
         Node(
             package='mecanum_swarm',
             executable='distributed_swarm',
-            name='distributed_swarm_controller',
+            name=f'distributed_swarm_controller_{robot_name.lower()}',
             output='screen'
         ),
     ])
